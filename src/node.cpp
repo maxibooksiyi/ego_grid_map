546215816c3f93611e35dc7bf17d860bec48c9d7
maxibooksiyi <1372740910@qq.com>
2024-02-28 21:28:33 +0800

    参考https://blog.csdn.net/chunchun2021/article/details/134535140添加了一个node.cpp，这样可以起起来一个ros节点，有一个可执行文件，之前的功能包没有可执行文件只是一个库，目前编译没有问题。
    参考https://blog.csdn.net/chunchun2021/article/details/134535140添加了一个node.cpp，这样可以起起来一个ros节点，有一个可执行文件，之前的功能包没有可执行文件只是一个库，目前编译没有问题。

---
+++ src/node.cpp
@@ -0,0 +1,19 @@
+#include <ros/ros.h>
+#include "plan_env/grid_map.h"
+
+
+GridMap::Ptr   grid_map_test_ ; 
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "gridmap_test_node");
+    ros::NodeHandle nh_("~");
+
+    grid_map_test_.reset(new GridMap) ;
+    grid_map_test_->initMap(nh_) ; 
+
+    ros::spin();
+  
+    return 0;
+}
+
